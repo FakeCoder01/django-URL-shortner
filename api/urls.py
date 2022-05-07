26e2b276b82ed89fa4aab78a605c036128c7efa7
FakeCoder01 <97719684+FakeCoder01@users.noreply.github.com>
2022-05-07 19:33:04 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
+from .views import URL_SHORTNER
 urlpatterns = [
-
-    path('', views.short, name='index')
+    path('api-auth/', include('rest_framework.urls')),
+    path('', views.short, name='index'),
+    path('url/', URL_SHORTNER.as_view(), name='api'),
 ]
