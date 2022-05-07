e2cd6392423d07ac6362810b96a3d27c0c2eacec
FakeCoder01 <97719684+FakeCoder01@users.noreply.github.com>
2022-05-07 19:32:41 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -5,6 +5,12 @@ from .models import links
 from django.utils.crypto import get_random_string
 import  datetime
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import LinkSerializers
+from .models import links, authTokens
+
 # Create your views here.
 
 def shorten_url(link):
@@ -26,3 +32,50 @@ def short(request):
         return JsonResponse(json.dumps({'surl': lid,}), content_type="application/json", safe=False) 
     else:
         return redirect('/?e=PR') 
+
+
+class URL_SHORTNER(APIView):
+    
+    def get(self, request, *args, **kwargs):
+
+        try:
+            api_key = request.GET['api_key']
+            link = request.GET['link']
+        except:
+            return Response({'error' : 'feilds not set'})
+     
+        if authTokens.objects.filter(api_key=api_key).exists():
+
+            if links.objects.filter(link=link).exists():
+                qs = links.objects.filter(link=link).first()
+                data = {
+                    'lid': qs.lid,
+                    'link': link,
+                    'trough': qs.trough,
+                    'api_key' : api_key,
+                    'created_on' : qs.created_on
+                }
+                return Response(data)
+            else:
+                lid = get_random_string(6)
+                while links.objects.filter(lid=lid).exists():
+                    lid = get_random_string(6)
+                data = {
+                    'lid': lid,
+                    'link': link,
+                    'trough': 'api',
+                    'api_key' : api_key,
+                    'created_on' : datetime.datetime.now()
+                }
+                serializer = LinkSerializers(data=data)
+
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data)
+                else:
+                    return Response(serializer.errors)
+        else:
+            return Response({'error':'wrong api_key'}) 
+
+
+
